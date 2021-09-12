69bcae6d89167973a79d1ff1be1376eb657dba80
djosh <darrenjcarrillo@gmail.com>
2021-09-12 09:58:05 -0700

    Delete book

--- app.js
+++ app.js
@@ -47,6 +47,14 @@ UI.prototype.showAlert = function (message, className) {
   }, 3000);
 };
 
+// Delete Book
+UI.prototype.deleteBook = function (target) {
+  if (target.className === "delete") {
+    target.parentElement.parentElement.remove();
+  } else {
+  }
+};
+
 // Clear Fields
 UI.prototype.clearFields = function () {
   document.getElementById("title").value = "";
@@ -54,7 +62,7 @@ UI.prototype.clearFields = function () {
   document.getElementById("isbn").value = "";
 };
 
-// Event Listeners
+// Event Listeners for add book
 document.getElementById("book-form").addEventListener("submit", function (e) {
   // Get form values
   const title = document.getElementById("title").value,
@@ -66,6 +74,8 @@ document.getElementById("book-form").addEventListener("submit", function (e) {
 
   // Instantiate UI
   const ui = new UI();
+
+  console.log(ui);
 
   // Validate
   if (title === "" || author === "" || isbn === "") {
@@ -81,6 +91,20 @@ document.getElementById("book-form").addEventListener("submit", function (e) {
     // Clear fields
     ui.clearFields();
   }
+
+  e.preventDefault();
+});
+
+// Event Listener for delete
+document.getElementById("book-list").addEventListener("click", function (e) {
+  // Instantiate UI
+  const ui = new UI();
+
+  // Delete Book
+  ui.deleteBook(e.target);
+
+  // Show message
+  ui.showAlert("Book Removed", "success");
 
   e.preventDefault();
 });
